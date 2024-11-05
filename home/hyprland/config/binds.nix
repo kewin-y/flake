f272fbbeae4ca271670284d6ae40654e395f4234
Kevin <jekevin964@gmail.com>
2024-11-04 21:42:14 -0500

    rewrite screenshot script
    ok
    
    ok

--- home/hyprland/config/binds.nix
+++ home/hyprland/config/binds.nix
@@ -33,39 +33,7 @@ in {
         ", XF86MonBrightnessDown, exec, ${pkgs.brillo}/bin/brillo -q -U 10"
       ];
       bind = let
-        # credit: hyprdots
-        screenshot = pkgs.writeShellScriptBin "screenshot" ''
-          if [ -z "$XDG_PICTURES_DIR" ] ; then
-              XDG_PICTURES_DIR="$HOME/Pictures"
-          fi
-
-          swpy_dir="$HOME/.config/swappy"
-          save_dir="''${2:-$XDG_PICTURES_DIR/Screenshots}"
-          save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
-          temp_screenshot="/tmp/screenshot.png"
-
-          mkdir -p $save_dir
-          mkdir -p $swpy_dir
-          echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
-
-          case $1 in
-          p)
-              ${pkgs.grimblast}/bin/grimblast copysave screen $temp_screenshot ; ${pkgs.swappy}/bin/swappy -f $temp_screenshot ;;
-          s)
-              ${pkgs.grimblast}/bin/grimblast --freeze copysave area $temp_screenshot ; ${pkgs.swappy}/bin/swappy -f $temp_screenshot ;;
-          m)
-              ${pkgs.grimblast}/bin/grimblast copysave output $temp_screenshot ; ${pkgs.swappy}/bin/swappy -f $temp_screenshot ;;
-          *)
-              exit ;;
-          esac
-
-          rm "$temp_screenshot"
-
-          if [ -f "$save_dir/$save_file" ] ; then
-              notify-send "Screenshot" "Screenshot saved in $save_dir" -i $save_dir/$save_file
-          fi
-        '';
-
+        screenshot = import ../scripts/screenshot.nix {inherit pkgs;};
         workspace = num: "${mainMod}, ${num}, workspace, ${num}";
         moveToWorkspace = num: "${mainMod} SHIFT, ${num}, movetoworkspace, ${num}";
         moveToWorkspaceSilent = num: "${mainMod} ALT, ${num}, movetoworkspacesilent, ${num}";
@@ -87,8 +55,8 @@ in {
 
           # Screenshot
           "${mainMod} SHIFT, S, exec, ${screenshot}/bin/screenshot s"
-          "${mainMod} ALT, S, exec, ${screenshot}/bin/screenshot m"
-          ",print, exec, ${screenshot}/bin/screenshot p"
+          "${mainMod} ALT, S, exec, ${screenshot}/bin/screenshot f"
+          ",print, exec, ${screenshot}/bin/screenshot f"
 
           # Focus
           "${mainMod}, h, movefocus, l"
