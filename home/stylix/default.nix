fc95a4eb58d6312e1cdc0fc869f8d88306b8f41b
Kevin <jekevin964@gmail.com>
2024-11-10 23:37:47 -0500

    theme: add material-darker colorscheme
    ok

--- home/stylix/default.nix
+++ home/stylix/default.nix
@@ -9,7 +9,7 @@
     # Placeholder (literally doesn't matter)
     image = config.wallpaper;
 
-    base16Scheme = ./themes/oxocarbon.yaml;
+    base16Scheme = ./themes/material-darker.yaml;
     polarity = "dark";
 
     cursor = {
