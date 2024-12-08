42b484dc4669fff1390c08ff4bafe670f1f5ac68
Kevin <jekevin964@gmail.com>
2024-12-07 22:48:25 -0500

    .

--- home/default.nix
+++ home/default.nix
@@ -39,7 +39,7 @@
   };
 
   config = {
-    wallpaper = ../wallpapers/wallhaven-4oem1l.jpg;
-    theme = "paradise";
+    wallpaper = ../wallpapers/nanmokaken.jpg;
+    theme = "rose-pine-dawn";
   };
 }
