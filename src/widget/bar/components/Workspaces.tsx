2384c0fb7d3c7a087337e7c52453ca4f7600e8d6
Kevin <jekevin964@gmail.com>
2024-12-21 18:14:04 -0500

    pill workspaces

--- src/widget/bar/components/Workspaces.tsx
+++ src/widget/bar/components/Workspaces.tsx
@@ -1,12 +1,14 @@
 import Hyprland from "gi://AstalHyprland";
+import Gtk from "gi://Gtk?version=3.0";
 
 export default function Workspaces() {
   const hypr = Hyprland.get_default();
 
   return (
-    <box className="workspaces" spacing={5}>
+    <box className="workspaces" spacing={8}>
       {Array.from({ length: 5 }, (_, i) => i + 1).map((i) => (
         <button
+          valign={Gtk.Align.CENTER}
           setup={(self) => {
             self.hook(hypr, "event", () => {
               self.toggleClassName("active", hypr.focusedWorkspace.id === i);
@@ -18,7 +20,6 @@ export default function Workspaces() {
           }}
           onClick={() => hypr.message_async(`dispatch workspace ${i}`, null)}
         >
-          <label label={`${i}`} />
         </button>
       ))}
     </box>
