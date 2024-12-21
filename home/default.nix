31dcd8234e26e7b7ab17701d2963b18dfb898434
Kevin <jekevin964@gmail.com>
2024-12-20 21:07:54 -0500

    .

--- home/default.nix
+++ home/default.nix
@@ -39,7 +39,7 @@
   };
 
   config = {
-    wallpaper = ../wallpapers/mountains.jpg;
-    theme = "paradise";
+    wallpaper = ../wallpapers/nanmokaken.jpg;
+    theme = "rose-pine-dawn";
   };
 }
--- home/stylix/default.nix
+++ home/stylix/default.nix
@@ -10,7 +10,7 @@
     image = config.wallpaper;
 
     base16Scheme = ./themes/${config.theme}.yaml;
-    polarity = "dark";
+    polarity = "light";
 
     cursor = {
       name = "phinger-cursors-light";
@@ -48,7 +48,7 @@
   gtk = {
     enable = true;
     iconTheme = {
-      name = "Papirus-Dark";
+      name = "Papirus-Light";
       package = pkgs.papirus-icon-theme.override {color = "blue";};
     };
   };
