1f806018e9377a8c99f1d40fe315196a89732b7a
Kevin <jekevin964@gmail.com>
2024-12-22 11:55:38 -0500

    .

--- src/widget/bar/components/Workspaces.tsx
+++ src/widget/bar/components/Workspaces.tsx
@@ -5,7 +5,7 @@ export default function Workspaces() {
   const hypr = Hyprland.get_default();
 
   return (
-    <box className="workspaces" spacing={4}>
+    <box className="workspaces" spacing={2}>
       {Array.from({ length: 5 }, (_, i) => i + 1).map((i) => (
         <button
           valign={Gtk.Align.CENTER}
