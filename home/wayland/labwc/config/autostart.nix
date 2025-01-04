dbcdee41a1e26e46c4dde87af3d8c5a3b79b20ab
Kevin <jekevin964@gmail.com>
2025-01-03 20:49:32 -0500

    autostart config

---
+++ home/wayland/labwc/config/autostart.nix
@@ -0,0 +1,8 @@
+{pkgs, ...}: {
+  home.file.".config/labwc/autostart".text = ''
+    swaybg -i ~/flake/wallpapers/leaving-breeze.jpg -m fill >/dev/null 2>&1 &
+    mako >/dev/null 2>&1 &
+    waybar >/dev/null 2>&1 &
+    "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
+  '';
+}
--- home/wayland/labwc/config/default.nix
+++ home/wayland/labwc/config/default.nix
@@ -1,3 +1,8 @@
 {
-  imports = [./rc.nix ./themerc.nix ./menu.nix];
+  imports = [
+    ./rc.nix
+    ./themerc.nix
+    ./menu.nix
+    ./autostart.nix
+  ];
 }
