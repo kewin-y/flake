5de6b64c276dd653c841a95d4995931ce2e2756a
Kevin <jekevin964@gmail.com>
2025-12-10 00:22:13 -0500

    add wlsunset

--- home/config/wayland/labwc/config/autostart.nix
+++ home/config/wayland/labwc/config/autostart.nix
@@ -5,6 +5,7 @@
 }: {
   home.file.".config/labwc/autostart".text = ''
     swaybg -i ${config.wallpaper} -m fill >/dev/null 2>&1 &
+    ${pkgs.wlsunset}/bin/wlsunset -l 43.7 -L -79.4 -t 3500 -T 6500 &
     mako >/dev/null 2>&1 &
     waybar >/dev/null 2>&1 &
     ghostty --gtk-single-instance=true --quit-after-last-window-closed=false --initial-window=false
