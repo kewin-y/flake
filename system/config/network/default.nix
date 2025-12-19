4f82d5837b338573c294f2e7d4c0def79020dcce
Kevin <jekevin964@gmail.com>
2025-12-18 23:52:20 -0500

    flake update

--- system/config/network/default.nix
+++ system/config/network/default.nix
@@ -1,6 +1,16 @@
 {
-  networking.networkmanager = {
-    enable = true;
-    appendNameservers = ["1.1.1.1"];
+  networking = {
+    networkmanager = {
+      enable = true;
+      appendNameservers = ["1.1.1.1"];
+    };
+    stevenblack = {
+      enable = true;
+      block = [
+        "fakenews"
+        "gambling"
+        "porn"
+      ];
+    };
   };
 }
