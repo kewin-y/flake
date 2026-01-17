c6eab847daaf083c4295bb5013bad9d851ec3b4b
Kevin <jekevin964@gmail.com>
2026-01-17 12:44:38 -0500

    help

--- system/config/network/default.nix
+++ system/config/network/default.nix
@@ -8,6 +8,7 @@
       enable = true;
       appendNameservers = ["1.1.1.1"];
     };
+
     stevenblack = {
       enable = true;
       block = [
@@ -17,5 +18,19 @@
         "social"
       ];
     };
+
+    extraHosts = ''
+      0.0.0.0 wallhaven.cc
+      0.0.0.0 coomer.st
+      0.0.0.0 cosplaytele.com
+      0.0.0.0 open.spotify.com
+      0.0.0.0 spotify.com
+      0.0.0.0 soundcloud.com
+      0.0.0.0 nitter.net
+      0.0.0.0 nitter.net
+      0.0.0.0 mangakatana.com
+      0.0.0.0 mangadex.org
+      0.0.0.0 mangabuddy.com
+    '';
   };
 }
