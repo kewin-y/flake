7aca359d50b6e7675e45c07b8f6d42e67d2efe93
Kevin <jekevin964@gmail.com>
2026-02-04 11:04:38 -0500

    remove certain hosts (using pihole now)

--- system/config/network/default.nix
+++ system/config/network/default.nix
@@ -17,17 +17,9 @@
     };
 
     extraHosts = ''
-      0.0.0.0 wallhaven.cc
-      0.0.0.0 coomer.st
-      0.0.0.0 cosplaytele.com
       0.0.0.0 open.spotify.com
       0.0.0.0 spotify.com
       0.0.0.0 soundcloud.com
-      0.0.0.0 nitter.net
-      0.0.0.0 nitter.net
-      0.0.0.0 mangakatana.com
-      0.0.0.0 mangadex.org
-      0.0.0.0 mangabuddy.com
       0.0.0.0 youtube.com
       0.0.0.0 www.youtube.com
     '';
