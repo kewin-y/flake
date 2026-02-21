84177a294305fd993490d9f8dcb39031d32a166b
Kevin <jekevin964@gmail.com>
2026-02-20 20:34:10 -0500

    fix: swaybg typo

--- config/home/labwc/autostart.nix
+++ config/home/labwc/autostart.nix
@@ -6,7 +6,7 @@
 }: {
     hjem.users.${globals.user}.files = {
         ".config/labwc/autostart".text = ''
-            {$pkgs.swaybg}/bin/swaybg -i ${config.wallpaper} -m fill >/dev/null 2>&1 &
+            ${pkgs.swaybg}/bin/swaybg -i ${config.wallpaper} -m fill >/dev/null 2>&1 &
 
             ${pkgs.wlsunset}/bin/wlsunset -l 43.7 -L -79.4 -t 3500 -T 6500 &
 
