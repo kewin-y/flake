bfeab8aa371e02faff225d711dfb9d8323cc7a45
Kevin <jekevin964@gmail.com>
2026-03-12 19:57:21 -0400

    improve nvim contrast

--- wrapped/neovim/lua/plugins/tinted.lua
+++ wrapped/neovim/lua/plugins/tinted.lua
@@ -5,6 +5,22 @@ return {
         lazy = false,
         opts = {
             default_scheme = "base16-flexoki-dark",
+            highlights = {
+                overrides = function(_)
+                    return {
+                        LineNr = { fg = "grey" },
+                        LineNrAbove = { fg = "grey" },
+                        LineNrBelow = { fg = "grey" },
+                        NonText = { fg = "grey" },
+                        EndOfBuffer = { fg = "grey" },
+                        SignColumn = { fg = "grey" },
+                        WinSeparator = { fg = "grey" },
+                        PmenuSel = { bg = "darkest_grey" },
+                        MiniPickMatchCurrent = { bg = "darkest_grey" },
+                        MiniPickMatchMarked = { bg = "dark_grey" },
+                    }
+                end,
+            },
         },
     },
 }
