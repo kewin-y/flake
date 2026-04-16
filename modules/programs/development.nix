3aab842a6da6084c000f90a3bd96f25c929138a3
Kevin <jekevin964@gmail.com>
2026-04-16 10:58:36 -0400

    format

--- modules/programs/development.nix
+++ modules/programs/development.nix
@@ -11,23 +11,24 @@
 
   # For stuff I'm too lazy to make a devshell for every time
   # Coding agents need LSPs so I'll keep these here
-  environment.systemPackages = lib.attrValues {
-    inherit
-      (pkgs)
-      typst
-      nodejs
-      pnpm
-      prettierd
-      typescript-language-server
-      typescript-go
-      tailwindcss-language-server
-      bun
-      opencode
-      valgrind-light
-      clang-tools
-      cmake
-      code-cursor
-      vscode
-      ;
-  };
+  environment.systemPackages =
+    lib.attrValues {
+      inherit
+        (pkgs)
+        typst
+        nodejs
+        pnpm
+        prettierd
+        typescript-language-server
+        typescript-go
+        tailwindcss-language-server
+        bun
+        valgrind-light
+        clang-tools
+        cmake
+        code-cursor
+        vscode
+        opencode
+        ;
+    };
 }
