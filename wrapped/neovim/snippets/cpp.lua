be9a53e64ac3175b6e1b33dd7db1a4b6b0f2e1e9
Kevin <jekevin964@gmail.com>
2026-05-06 12:25:34 -0400

    add more cpp snippets

--- wrapped/neovim/snippets/cpp.lua
+++ wrapped/neovim/snippets/cpp.lua
@@ -1,4 +1,10 @@
 ---@diagnostic disable: undefined-global
+
+local function header_guard()
+  local name = vim.fn.expand("%:t")
+  return name:gsub("%.", "_"):gsub("[^%w_]", "_"):upper()
+end
+
 return {
   s(
     { trig = "cp", name = "Competitive Programming Template", dscr = "Standard C++ CP boilerplate" },
@@ -22,4 +28,34 @@ return {
       }),
     }
   ),
+
+  s("guard", {
+    t("#ifndef "),
+    f(header_guard),
+    t({ "", "#define " }),
+    f(header_guard),
+    t({ "", "", "" }),
+    t({ "", "#endif" }),
+  }),
+
+  s("for", {
+    t("for ("),
+    i(1, "int"),
+    t(" "),
+    i(2, "i"),
+    t(" = "),
+    i(3, "0"),
+    t("; "),
+    rep(2),
+    t(" < "),
+    i(4, "n"),
+    t("; "),
+    rep(2),
+    t(" "),
+    i(5, "++"),
+    t(") {"),
+    t({ "", "  " }),
+    i(0),
+    t({ "", "}" }),
+  }),
 }
