65668ac985f660d1e34d4dffb37c8c7f3ebad966
kevin <jekevin964@gmail.com>
2026-08-04 14:24:45 -0400

    alright

--- modules/programs/development.nix
+++ modules/programs/development.nix
@@ -2,40 +2,69 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  arduinoIDE = pkgs.symlinkJoin {
+    name = "arduino-ide-x11";
+    paths = [pkgs.arduino-ide];
+    nativeBuildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/arduino-ide --set ELECTRON_OZONE_PLATFORM_HINT x11
+    '';
+  };
+in {
   programs.nix-ld.enable = true;
 
   environment.sessionVariables = {
     EDITOR = "nvim";
   };
 
-  # For stuff I'm too lazy to make a devshell for every time
-  # Coding agents need LSPs so I'll keep these here
-  environment.systemPackages = lib.attrValues {
-    inherit
-      (pkgs)
-      typst
-      nodejs
-      pnpm
-      prettierd
-      typescript-language-server
-      typescript-go
-      tailwindcss-language-server
-      bun
-      valgrind-light
-      clang-tools
-      cmake
-      ty
-      ruff
-      nh
-      pi-coding-agent
-      cargo
-      clippy
-      rust-analyzer
-      rustc
-      rustfmt
-      tinymist
-      typstyle
-      ;
-  };
+  environment.systemPackages = let
+    pythonPackages = ps:
+      with ps; [
+        jupyter
+        notebook
+        ipykernel
+        numpy
+        pandas
+        matplotlib
+        torch
+      ];
+    pythonEnv = pkgs.python3.withPackages pythonPackages;
+  in
+    lib.attrValues {
+      inherit
+        (pkgs)
+        typst
+        nodejs
+        pnpm
+        prettierd
+        typescript-language-server
+        typescript-go
+        tailwindcss-language-server
+        bun
+        valgrind-light
+        clang-tools
+        cmake
+        ty
+        ruff
+        nh
+        cargo
+        clippy
+        rust-analyzer
+        rustc
+        rustfmt
+        tinymist
+        typstyle
+        picocom
+        psmisc
+
+        pi-coding-agent
+        codex
+        t3code
+        ;
+      arduinoIDE = arduinoIDE;
+    }
+    ++ [
+      pythonEnv
+    ];
 }
--- modules/system/user.nix
+++ modules/system/user.nix
@@ -2,6 +2,13 @@
   users.users.${base.user} = {
     isNormalUser = true;
     description = "Kevin";
-    extraGroups = ["networkmanager" "wheel" "video" "docker" "syncthing"];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "video"
+      "docker"
+      "syncthing"
+      "dialout"
+    ];
   };
 }
