b57d2bfb039864f08309760685e2897bb8744b28
kevin <jekevin964@gmail.com>
2026-08-05 11:35:04 -0400

    swapfile for rabbit

--- hosts/rabbit/configuration.nix
+++ hosts/rabbit/configuration.nix
@@ -13,5 +13,12 @@
     bootloader = "systemd-boot";
   };
 
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 16 * 1024; # 16 GiB
+    }
+  ];
+
   system.stateVersion = "25.05";
 }
